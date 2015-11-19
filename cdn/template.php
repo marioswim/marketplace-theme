6170d97953f0b4e29cbd46555522142d4b1b5ebd
Mario Quesada <marioquesada2@gmail.com>
2015-11-19 12:58:08 +0100

    Productos añadidos a la portada, falta tooltip para el titulo

--- cdn/template.php
+++ cdn/template.php
@@ -3,3 +3,11 @@
  * @file
  * The primary PHP file for this theme.
  */
+function MarketPlace_preprocess_page(&$vars) 
+{ 
+	if(drupal_is_front_page()) 
+	{ 
+		unset($vars['page']['content']['system_main']['default_message']); //will remove message "no front page content is created" 
+		drupal_set_title(''); //removes welcome message (page title) 
+	}
+} 
