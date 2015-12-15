de5df8620f84e591262806dd6d9f90957058f65e
Mario Quesada <marioquesada2@gmail.com>
2015-12-15 11:11:59 +0100

    sobreescrito la vista de los campos para que no salgan los :. Modificado los breadcrumbs para que aparezca el nodo del que cuelgan (hardocode)

--- cdn/template.php
+++ cdn/template.php
@@ -10,7 +10,93 @@ function MarketPlace_preprocess_page(&$vars)
 		unset($vars['page']['content']['system_main']['default_message']); //will remove message "no front page content is created" 
 		drupal_set_title(''); //removes welcome message (page title) 
 	}
+
+	
 }
+
+function MarketPlace_field($variables)
+{
+	//dpm($variables);
+	$output='';
+	$output.='<div class="'.$variables["classes"].'">';
+
+		 if(!empty($variables["label"]))
+			$output.='<div class="field-label">'.$variables["label"].'</div>';
+		
+
+		$output.='<div class="field-items">';
+			$output.='<div class="field-item">';
+
+				 $output.=$variables["items"][0]["#markup"]; 
+				
+			$output.='</div>';
+			
+		$output.='</div>';
+	$output.='</div>';
+	
+	return $output;
+}
+
+function MarketPlace_breadcrumb($variables)
+{
+	dpm($variables);
+	$breadcrumb='
+		<ol class="breadcrumb">
+			<li>'.$variables["breadcrumb"][0].'</li>';
+	$node=menu_get_object();
+	$start=strpos($variables["breadcrumb"][0], '"');
+	$end=strripos($variables["breadcrumb"][0], '"');
+
+	$lan=substr($variables["breadcrumb"][0], $start+1,$end-$start-1);
+
+	switch (node_type_get_name($node)) 
+	{
+	 	case 'Research Group':
+	 	for($i=1;$i<count($variables['breadcrumb'])-1;$i++)
+ 			{
+ 				if(isset($breadcrumb["breadcrumb"][$i]["class"]))
+ 					$breadcrumb.='<li ="'.$variables["breadcrumb"][$i]["class"][0].'">'.$variables["breadcrumb"][$i]["data"].'</li>';
+ 				else
+ 					$breadcrumb.='<li>'.$variables["breadcrumb"][$i]["data"].'</li>';
+ 			}
+ 			$path=drupal_get_path_alias("research-group");
+
+	 		$breadcrumb.='<li><a href="'.$lan.'/'.$path.'">'.t("Research Groups").'</a></li>';	
+	 		$breadcrumb.='<li class ="'.$variables["breadcrumb"][count($variables["breadcrumb"])-1]["class"][0].'">'.$variables["breadcrumb"][count($variables["breadcrumb"])-1]["data"].'</li>';	
+	 		break;
+	 	
+	 	case 'Product':
+	 	for($i=1;$i<count($variables['breadcrumb'])-1;$i++)
+ 			{
+ 				if(isset($breadcrumb["breadcrumb"][$i]["class"]))
+ 					$breadcrumb.='<li ="'.$variables["breadcrumb"][$i]["class"][0].'">'.$variables["breadcrumb"][$i]["data"].'</li>';
+ 				else
+ 					$breadcrumb.='<li>'.$variables["breadcrumb"][$i]["data"].'</li>';
+ 			}
+
+ 			$path=drupal_get_path_alias("list-products");
+
+	 		$breadcrumb.='<li><a href="'.$lan.'/'.$path.'">'.t("Products").'</a></li>';	
+	 		$breadcrumb.='<li class ="'.$variables["breadcrumb"][count($variables["breadcrumb"])-1]["class"][0].'">'.$variables["breadcrumb"][count($variables["breadcrumb"])-1]["data"].'</li>';	
+	 		break;
+
+ 		default:
+ 			
+ 			for($i=1;$i<count($variables['breadcrumb']);$i++)
+ 			{
+ 				if(isset($breadcrumb["breadcrumb"][$i]["class"]))
+ 					$breadcrumb.='<li ="'.$variables["breadcrumb"][$i]["class"][0].'">'.$variables["breadcrumb"][$i]["data"].'</li>';
+ 				else
+ 					$breadcrumb.='<li>'.$variables["breadcrumb"][$i]["data"].'</li>';
+ 			}
+
+ 			break;
+	} 
+
+	$breadcrumb.="</ol>";
+	return $breadcrumb;
+}
+
 /*
 function MarketPlace_breadcrumb($variables) {
   $breadcrumb = $variables['breadcrumb'];
